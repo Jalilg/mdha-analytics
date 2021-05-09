ad561a2d60c03dcbddc0f73f83f65bbe7d67eab4
Jalilg <35570605+Jalilg@users.noreply.github.com>
2021-05-09 10:55:54 -0400

    updated dashboard

--- js/dashboard_charts.js
+++ js/dashboard_charts.js
@@ -17,47 +17,119 @@ var orgOverview;
 
 $.ajax('https://raw.githubusercontent.com/Jalilg/mdha-analytics/main/dashboard_output/es_only_duration.json').done(function (dat) {
   esOnlyDuration = JSON.parse(dat)
-  return esOnlyDuration
+
+  var labels = esOnlyDuration.map(function(e) {
+    return e.label;
+  });
+  var data = esOnlyDuration.map(function(e) {
+    return e.y;
+  });
+
+  makeEsOnlyDuration(labels, data)
 });
 
 $.ajax('https://raw.githubusercontent.com/Jalilg/mdha-analytics/main/dashboard_output/es_only_providers.json').done(function (dat) {
   esOnlyProvider = JSON.parse(dat)
-  return esOnlyProvider
+  
+  var labels = esOnlyProvider.map(function(e) {
+    return e.label;
+  });
+  var data = esOnlyProvider.map(function(e) {
+    return e.y;
+  });
+
+  makeEsOnlyProvider(labels, data)
 });
 
 $.ajax('https://raw.githubusercontent.com/Jalilg/mdha-analytics/main/dashboard_output/housed_only_duration.json').done(function (dat) {
   housedOnlyDuration = JSON.parse(dat)
-  return housedOnlyDuration
+  
+  var labels = housedOnlyDuration.map(function(e) {
+    return e.label;
+  });
+  var data = housedOnlyDuration.map(function(e) {
+    return e.y;
+  });
+
+  makeHousedOnlyDuration(labels, data)
 });
 
 $.ajax('https://raw.githubusercontent.com/Jalilg/mdha-analytics/main/dashboard_output/housed_only_providers.json').done(function (dat) {
   housedOnlyProvider = JSON.parse(dat)
-  return housedOnlyProvider
+    
+  var labels = housedOnlyProvider.map(function(e) {
+    return e.label;
+  });
+  var data = housedOnlyProvider.map(function(e) {
+    return e.y;
+  });
+
+  makeHousedOnlyProvider(labels, data)
 });
 
 $.ajax('https://raw.githubusercontent.com/Jalilg/mdha-analytics/main/dashboard_output/homeless_rehoused_duration.json').done(function (dat) {
   homelessRehousedDuration = JSON.parse(dat)
-  return homelessRehousedDuration
+  
+  var labels = homelessRehousedDuration.map(function(e) {
+    return e.label;
+  });
+  var data = homelessRehousedDuration.map(function(e) {
+    return e.y;
+  });
+
+  makeHomelessRehousedDuration(labels, data)
 });
 
 $.ajax('https://raw.githubusercontent.com/Jalilg/mdha-analytics/main/dashboard_output/homeless_rehoused_providers.json').done(function (dat) {
   homelessRehousedProvider = JSON.parse(dat)
-  return homelessRehousedProvider
+    
+  var labels = homelessRehousedProvider.map(function(e) {
+    return e.label;
+  });
+  var data = homelessRehousedProvider.map(function(e) {
+    return e.y;
+  });
+
+  makeHomelessRehousedProvider(labels, data)
 });
 
 $.ajax('https://raw.githubusercontent.com/Jalilg/mdha-analytics/main/dashboard_output/recurrence_duration.json').done(function (dat) {
   recurrenceDuration = JSON.parse(dat)
-  return recurrenceDuration
+  
+  var labels = recurrenceDuration.map(function(e) {
+    return e.label;
+  });
+  var data = recurrenceDuration.map(function(e) {
+    return e.y;
+  });
+
+  makeRecurrenceDuration(labels, data)
 });
 
 $.ajax('https://raw.githubusercontent.com/Jalilg/mdha-analytics/main/dashboard_output/recurrence_providers.json').done(function (dat) {
   recurrenceProvider = JSON.parse(dat)
-  return recurrenceProvider
+    
+  var labels = recurrenceProvider.map(function(e) {
+    return e.label;
+  });
+  var data = recurrenceProvider.map(function(e) {
+    return e.y;
+  });
+
+  makeRecurrenceProvider(labels, data)
 });
 
 $.ajax('https://raw.githubusercontent.com/Jalilg/mdha-analytics/main/dashboard_output/org_load.json').done(function (dat) {
   orgOverview = JSON.parse(dat)
-  return orgOverview
+    
+  var labels = orgOverview.map(function(e) {
+    return e.label;
+  });
+  var data = orgOverview.map(function(e) {
+    return e.y;
+  });
+
+  makeOrgOverview(labels, data)
 });
 
 var tooltipSettings = {   
@@ -72,235 +144,266 @@ var tooltipSettingsDur = {
             cornerRadius: 4      
         }
 
-window.onload = function () {
+var makeOrgOverview = function (labels, data) {
+  var ctx = document.getElementById('orgOverviewChart').getContext('2d');
+  var config = {
+     type: 'bar',
+     data: {
+        labels: labels,
+        datasets: [{
+           data: data,
+           backgroundColor: "#6e707e"
+        }]
+     },
+     options: {
+          legend: {
+              display: false
+          },
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          },
+          maintainAspectRatio: false,
+          responsive: true
+      }
+  };
 
-var chart = new CanvasJS.Chart(document.getElementById("orgOverviewChart"), {
-animationEnabled: true,
-    theme: "light2", // "light1", "light2", "dark1", "dark2"
-    toolTip : tooltipSettings,
-    title: {
-        text: ""
-    },
-    axisY: {
-        labelFontSize: 14,
-        titleFontSize: 16,
-        title: "# of Clients",
-        minimum: 0,
-        maximum: 6010,
-        margin: 20
-    },
-    axisX: {
-        labelFontSize: 14,
-        titleFontSize: 16,
-        interval: 1,
-        title: "Service Providers",
-        labelAngle: 300
-    },
-    data: [{
-        type: "column",
-        color: "#6e707e",
-        dataPoints: orgOverview
-    }]
-});
-chart.render();
-
-var chart1 = new CanvasJS.Chart(document.getElementById("esOnlyDurationChart"), {
-animationEnabled: true,
-    theme: "light2", // "light1", "light2", "dark1", "dark2"
-    toolTip : tooltipSettingsDur,
-    title: {
-        text: ""
-    },
-    axisY: {
-        title: "# of Clients",
-        minimum: 0,
-        maximum: 11000,
-    },
-    axisX: {
-        title: "# of days"
-    },
-    data: [{
-        type: "column",
-        color: "#db7027",
-        dataPoints: esOnlyDuration
-    }]
-});
-chart1.render();
-
-var chart2 = new CanvasJS.Chart(document.getElementById("esOnlyProviderChart"), {
-animationEnabled: true,
-    theme: "light2", // "light1", "light2", "dark1", "dark2"
-    toolTip : tooltipSettings,
-    title: {
-        text: ""
-    },
-    axisY: {
-        title: "# of Clients",
-        minimum: 0,
-        maximum: 5300,
-    },
-    axisX: {
-        interval: 1,
-        title: "Service Providers",
-        labelAngle: 300
-    },
-    data: [{
-        type: "column",
-        color: "#db7027",
-        dataPoints: esOnlyProvider
-    }]
-});
-chart2.render();
-
-var chart3 = new CanvasJS.Chart(document.getElementById("housedOnlyDurationChart"), {
-animationEnabled: true,
-    theme: "light2", // "light1", "light2", "dark1", "dark2"
-    toolTip : tooltipSettingsDur,
-    title: {
-        text: ""
-    },
-    axisY: {
-        title: "# of Clients",
-        minimum: 0,
-        maximum: 1200,
-    },
-    axisX: {
-        title: "# of days"
-    },
-    data: [{
-        type: "column",
-        color: "#244ba0",
-        dataPoints: housedOnlyDuration
-    }]
-});
-chart3.render();
-
-var chart4 = new CanvasJS.Chart(document.getElementById("housedOnlyProviderChart"), {
-animationEnabled: true,
-    theme: "light2", // "light1", "light2", "dark1", "dark2"
-    toolTip : tooltipSettings,
-    title: {
-        text: ""
-    },
-    axisY: {
-        title: "# of Clients",
-        minimum: 0,
-        maximum: 550,
-        margin: 10
-    },
-    axisX: {
-        interval: 1,
-        title: "Service Providers",
-        labelAngle: 300
-    },
-    data: [{
-        type: "column",
-        color: "#244ba0",
-        dataPoints: housedOnlyProvider
-    }]
-});
-chart4.render();
-
-var chart5 = new CanvasJS.Chart(document.getElementById("homelessRehousedDurationChart"), {
-animationEnabled: true,
-    theme: "light2", // "light1", "light2", "dark1", "dark2"
-    toolTip : tooltipSettingsDur,
-    title: {
-        text: ""
-    },
-    axisY: {
-        title: "# of Clients",
-        minimum: 0,
-        maximum: 35,
-    },
-    axisX: {
-        title: "# of days"
-    },
-    data: [{
-        type: "column",
-        color: "#244ba0",
-        dataPoints: homelessRehousedDuration
-    }]
-});
-chart5.render();
-
-var chart6 = new CanvasJS.Chart(document.getElementById("homelessRehousedProviderChart"), {
-animationEnabled: true,
-    theme: "light2", // "light1", "light2", "dark1", "dark2"
-    toolTip : tooltipSettings,
-    title: {
-        text: ""
-    },
-    axisY: {
-        title: "# of Clients",
-        minimum: 0,
-        maximum: 45,
-        margin: 20
-    },
-    axisX: {
-        interval: 1,
-        title: "Service Providers",
-        labelAngle: 300
-    },
-    data: [{
-        type: "column",
-        color: "#244ba0",
-        dataPoints: homelessRehousedProvider
-    }]
-});
-chart6.render();
-
-var chart5 = new CanvasJS.Chart(document.getElementById("recurrenceDurationChart"), {
-animationEnabled: true,
-    theme: "light2", // "light1", "light2", "dark1", "dark2"
-    toolTip : tooltipSettingsDur,
-    title: {
-        text: ""
-    },
-    axisY: {
-        title: "# of Clients",
-        minimum: 0,
-        maximum: 200
-    },
-    axisX: {
-        title: "# of days"
-    },
-    data: [{
-        type: "column",
-        color: "#5383c3",
-        dataPoints: recurrenceDuration
-    }]
-});
-chart5.render();
-
-var chart6 = new CanvasJS.Chart(document.getElementById("recurrenceProviderChart"), {
-animationEnabled: true,
-    theme: "light2", // "light1", "light2", "dark1", "dark2"
-    toolTip : tooltipSettings,
-    title: {
-        text: ""
-    },
-    axisY: {
-        title: "# of Clients",
-        minimum: 0,
-        maximum: 310,
-        margin: 20
-    },
-    axisX: {
-        interval: 1,
-        title: "Service Providers",
-        labelAngle: 300
-    },
-    data: [{
-        type: "column",
-        color: "#5383c3",
-        dataPoints: recurrenceProvider
-    }]
-});
-chart6.render();
-
-
-
+  var chart = new Chart(ctx, config);
+  return chart
 }
+
+var makeEsOnlyDuration = function (labels, data) {
+  var ctx = document.getElementById('esOnlyDurationChart').getContext('2d');
+  var config = {
+     type: 'bar',
+     data: {
+        labels: labels,
+        datasets: [{
+           data: data,
+           backgroundColor: "#db7027"
+        }]
+     },
+     options: {
+          legend: {
+              display: false
+          },
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          },
+          maintainAspectRatio: false,
+          responsive: true
+      }
+  };
+
+  var chart = new Chart(ctx, config);
+  return chart
+}
+
+var makeEsOnlyProvider = function (labels, data) {
+  var ctx = document.getElementById('esOnlyProviderChart').getContext('2d');
+  var config = {
+     type: 'bar',
+     data: {
+        labels: labels,
+        datasets: [{
+           data: data,
+           backgroundColor: "#db7027"
+        }]
+     },
+     options: {
+          legend: {
+              display: false
+          },
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          },
+          maintainAspectRatio: false,
+          responsive: true
+      }
+  };
+
+  var chart = new Chart(ctx, config);
+  return chart
+}
+
+var makeHousedOnlyDuration = function (labels, data) {
+  var ctx = document.getElementById('housedOnlyDurationChart').getContext('2d');
+  var config = {
+     type: 'bar',
+     data: {
+        labels: labels,
+        datasets: [{
+           data: data,
+           backgroundColor: "#244ba0"
+        }]
+     },
+     options: {
+          legend: {
+              display: false
+          },
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          },
+          maintainAspectRatio: false,
+          responsive: true
+      }
+  };
+
+  var chart = new Chart(ctx, config);
+  return chart
+}
+
+var makeHousedOnlyProvider = function (labels, data) {
+  var ctx = document.getElementById('housedOnlyProviderChart').getContext('2d');
+  var config = {
+     type: 'bar',
+     data: {
+        labels: labels,
+        datasets: [{
+           data: data,
+           backgroundColor: "#244ba0"
+        }]
+     },
+     options: {
+          legend: {
+              display: false
+          },
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          },
+          maintainAspectRatio: false,
+          responsive: true
+      }
+  };
+
+  var chart = new Chart(ctx, config);
+  return chart
+}
+
+var makeHomelessRehousedDuration = function (labels, data) {
+  var ctx = document.getElementById('homelessRehousedDurationChart').getContext('2d');
+  var config = {
+     type: 'bar',
+     data: {
+        labels: labels,
+        datasets: [{
+           data: data,
+           backgroundColor: "#244ba0"
+        }]
+     },
+     options: {
+          legend: {
+              display: false
+          },
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          },
+          maintainAspectRatio: false,
+          responsive: true
+      }
+  };
+
+  var chart = new Chart(ctx, config);
+  return chart
+}
+
+var makeHomelessRehousedProvider = function (labels, data) {
+  var ctx = document.getElementById('homelessRehousedProviderChart').getContext('2d');
+  var config = {
+     type: 'bar',
+     data: {
+        labels: labels,
+        datasets: [{
+           data: data,
+           backgroundColor: "#244ba0"
+        }]
+     },
+     options: {
+          legend: {
+              display: false
+          },
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          },
+          maintainAspectRatio: false,
+          responsive: true
+      }
+  };
+
+  var chart = new Chart(ctx, config);
+  return chart
+}
+
+var makeRecurrenceDuration = function (labels, data) {
+  var ctx = document.getElementById('recurrenceDurationChart').getContext('2d');
+  var config = {
+     type: 'bar',
+     data: {
+        labels: labels,
+        datasets: [{
+           data: data,
+           backgroundColor: "#5383c3"
+        }]
+     },
+     options: {
+          legend: {
+              display: false
+          },
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          },
+          maintainAspectRatio: false,
+          responsive: true
+      }
+  };
+
+  var chart = new Chart(ctx, config);
+  return chart
+}
+
+var makeRecurrenceProvider = function (labels, data) {
+  var ctx = document.getElementById('recurrenceProviderChart').getContext('2d');
+  var config = {
+     type: 'bar',
+     data: {
+        labels: labels,
+        datasets: [{
+           data: data,
+           backgroundColor: "#5383c3"
+        }]
+     },
+     options: {
+          legend: {
+              display: false
+          },
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          },
+          maintainAspectRatio: false,
+          responsive: true
+      }
+  };
+
+  var chart = new Chart(ctx, config);
+  return chart
+}
+
 
 
