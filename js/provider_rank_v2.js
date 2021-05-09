f750e1331bf10b9fb708eaa96ec3d7e0f2756031
Jalilg <35570605+Jalilg@users.noreply.github.com>
2021-05-09 08:51:07 -0400

    Update provider_rank_v2.js

--- js/provider_rank_v2.js
+++ js/provider_rank_v2.js
@@ -7,42 +7,46 @@ var data;
 $.ajax('https://raw.githubusercontent.com/Jalilg/mdha-analytics/main/rank_output/ServProvAggregagte20200701.json').done(function (dat) {
   provRank = JSON.parse(dat)
   console.log(provRank)
-  return provRank
-});
-console.log(provRank)
 
-window.onload = function () {
+  var labels = provRank.map(function(e) {
+    return e.label;
+  });
+  var data = provRank.map(function(e) {
+    return e.y;
+  });
 
-var labels = provRank.map(function(e) {
-return e.label;
-});
-var data = provRank.map(function(e) {
-   return e.y;
+  makeChart(labels, data)
 });
 
-var ctx = document.getElementById('myChart').getContext('2d');
-var config = {
-   type: 'bar',
-   data: {
-      labels: labels,
-      datasets: [{
-         data: data,
-         backgroundColor: "#db7027"
-      }]
-   },
-   options: {
-        legend: {
-            display: false
-        },
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        },
-        maintainAspectRatio: false,
-        responsive: true
-    }
-};
+console.log(labels)
+console.log(data)
 
-var chart = new Chart(ctx, config);
+var makeChart = function (labels, data) {
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var config = {
+     type: 'bar',
+     data: {
+        labels: labels,
+        datasets: [{
+           data: data,
+           backgroundColor: "#db7027"
+        }]
+     },
+     options: {
+          legend: {
+              display: false
+          },
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          },
+          maintainAspectRatio: false,
+          responsive: true
+      }
+  };
+
+  var chart = new Chart(ctx, config);
+  return chart
 }
+
