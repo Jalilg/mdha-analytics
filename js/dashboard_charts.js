040ab9fdfb752593a227fde51f5dc7b0e315c8e5
Jalilg <35570605+Jalilg@users.noreply.github.com>
2021-09-17 17:05:53 -0400

    added files

--- js/dashboard_charts.js
+++ js/dashboard_charts.js
@@ -130,6 +130,26 @@ $.ajax('https://raw.githubusercontent.com/Jalilg/mdha-analytics/main/dashboard_o
     makeOrgOverview(labels, data)
 });
 
+new Chart(document.getElementById("doughnut-chart"), {
+    type: 'doughnut',
+    data: {
+      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+      datasets: [
+        {
+          label: "Population (millions)",
+          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+          data: [2478,5267,734,784,433]
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Predicted world population (millions) in 2050'
+      }
+    }
+});
+
 var makeOrgOverview = function(labels, data) {
     var ctx = document.getElementById('orgOverviewChart').getContext('2d');
     var config = {
